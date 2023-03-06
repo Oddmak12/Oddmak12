48a8d0aa9f010202b4e0a688c0844cdbe0396d9c
Oddmak12 <126546274+Oddmak12@users.noreply.github.com>
2023-03-06 14:57:21 +0800

    Update 垃圾笔记.c

--- 垃圾笔记.c
+++ 垃圾笔记.c
@@ -138,3 +138,31 @@ int main()
 	}
 	return 0;
 }	
+认真学！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int odd[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,};
+	int hdb = sizeof(odd) / sizeof(odd[0]);
+	int left = 0, right = hdb - 1;
+	int temp;
+	scanf("%d", &temp);
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (odd[mid] > temp)
+			 right = mid-1;
+		else if (odd[mid] < temp)
+			 left= mid+1;
+		else
+
+		{
+			printf("get it %d", mid);
+			break;
+		}
+	}
+	if (left > right)
+		printf("找不到");
+}
+认真学！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
